265b1f3999d2af682615e4c7b249a94706c629da
jibin0518 <114150533+jibin0518@users.noreply.github.com>
2026-03-24 14:01:39 +0900

    0324

--- translate.py
+++ translate.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import keyboard
+import mouse
 
 def close(event=None):
     window.destroy()
@@ -20,22 +21,44 @@ box = canvas.create_rectangle(100, 100, 400, 300, outline="green", width=7, tags
 
 rec_pos = [100,100,400,300]
 last_rec_pos = [0,0,0,0]
-
+last_rec_pos = rec_pos
 box_in_ckick=False
+rec_mouse_pont = [0,0]
+
+def mos_pos(event):
+    global rec_mouse_pont
+    rec_mouse_pont[0]=event.x
+    rec_mouse_pont[1]=event.y
 
 def box_move(event):
     global rec_pos
     global last_rec_pos
-    x_lenght = rec_pos[2]-rec_pos[0]
-    y_lenght = rec_pos[3]-rec_pos[1]
-    x1 = rec_pos[0]+event.x-x_lenght
-    y1 = rec_pos[1]+event.y-y_lenght
-    x2 = rec_pos[2]+event.x-x_lenght
-    y2 = rec_pos[3]+event.y-y_lenght
+    global rec_mouse_pont
+    mos_gap = [rec_mouse_pont[0]-rec_pos[0],rec_mouse_pont[1]-rec_pos[1],
+               rec_pos[2]-rec_mouse_pont[0],rec_pos[3]-rec_mouse_pont[1]]
+    x1 = event.x-mos_gap[0]
+    y1 = event.y-mos_gap[1]
+    x2 = event.x+mos_gap[2]
+    y2 = event.y+mos_gap[3]
     canvas.coords("box",x1,y1,x2,y2)
-    print(x1,y1,x2,y2)
+    last_rec_pos = [x1,y1,x2,y2]
 
+def box_size_change(event):
+    global rec_pos
+    if(event.x<rec_pos[0]+10 and event.y<rec_pos[1]+10):#x<110 and y<110
+        print("우측 상단 :",event.x,event.y)
+    elif(event.x<rec_pos[0]+10 and event.y>rec_pos[3]-10):#x<110 and y>290
+        print("우측 하단 :",event.x,event.y)
+    elif(event.x>rec_pos[2]-10 and event.y<rec_pos[1]+10):#x>390 and y<110
+        print("좌측 상단 :",event.x,event.y)
+    elif(event.x>rec_pos[2]-10 and event.y>rec_pos[3]-10):#x>390 and y>290
+        print("좌측 하단 :",event.x,event.y)
 
+def mos_point_finish(event):#좌클릭 땜 -> 박스 위치 업데이트
+    global rec_mouse_pont
+    global rec_pos
+    rec_pos = last_rec_pos
+    
 def key_loop():
     if keyboard.is_pressed('esc'):
         print("꺼짐")
@@ -43,7 +66,10 @@ def key_loop():
         return
     window.after(100, key_loop)
 
+canvas.bind("<Button-1>", mos_pos)
 canvas.bind("<B1-Motion>", box_move)
+canvas.bind("<Motion>",box_size_change)
+canvas.bind("<ButtonRelease-1>",mos_point_finish)
 
 key_loop()
 window.mainloop()
